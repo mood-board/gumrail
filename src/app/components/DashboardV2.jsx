d71d2123b0f08c9846a0f69eaddc84edc3f48693
Ofonime Francis <ofonimeusoro01@gmail.com>
2024-08-29 11:05:11 +0200

    minor change

--- src/app/components/DashboardV2.jsx
+++ src/app/components/DashboardV2.jsx
@@ -4,6 +4,20 @@ export default function DashboardV2() {
   return (
     <div>
       <h1>DashboardV2</h1>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        fill="none"
+      >
+        <path
+          fill="#0073E6"
+          fill-rule="evenodd"
+          d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zM9.5 6.5h5c1.657 0 3 1.343 3 3v5c0 1.657-1.343 3-3 3h-5c-1.657 0-3-1.343-3-3v-5c0-1.657 1.343-3 3-3zm5.5 3.5h-3v1.5h3v2h-3v1.5h3v-5zm-5.5 0c-.276 0-.5.224-.5.5v4c0 .276.224.5.5.5h1.5v-5H9.5z"
+          clip-rule="evenodd"
+        />
+      </svg>
     </div>
   );
 }
